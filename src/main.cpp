e1484e1599ead78d48c509d67a750b8dc374bcb7
Alexandre Lewkowicz <lewkow_a@epita.fr>
2015-04-29 18:39:26 +0200

    Add main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 #include "graph.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    std::fstream fs;
+    fstream fs;
     if (argc >= 2) {
-        fs.open(argv[1], std::fstream::in);
+        fs.open(argv[1], fstream::in);
     } else {
         cerr << "Pass graph path to prgram" << endl;
         return 1;
     }
     graph g(fs);
-    cout << g.eccentricity(0) << endl;
+    for (unsigned i = 0; i < 10; ++i) {
+      cout << g.eccentricity(i * 100) << endl;
+    }
+    vector<unsigned> dists;
+    dists.reserve(g.nodes.size());
+    for (unsigned i = 0; i < 10; ++i) {
+      dists.assign(g.nodes.size(), 0);
+      g.distances(i * 100, dists);
+      cout << *max_element(dists.begin(), dists.end()) << endl;
+    }
     return 0;
 }
